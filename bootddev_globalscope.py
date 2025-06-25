d808304e92d37b426d20ad222fda59ce666a50bd
Al Mallari <almallari.dev@gmail.com>
2025-06-25 17:32:57 +0800

    added the incomplete buggy code

--- bootddev_globalscope.py
+++ bootddev_globalscope.py
@@ -19,3 +19,20 @@ Let's change how we are calculating our player's stats! The only thing we should
 Declare the variable player_level at the top of the global scope and set it to 4.
 """
 
+# ?
+
+# Don't touch below this line
+
+
+def calculate_health(modifier):
+    return player_level * modifier
+
+
+def calculate_primary_stats(armor_bonus, modifier):
+    return armor_bonus + modifier + player_level
+
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
+
