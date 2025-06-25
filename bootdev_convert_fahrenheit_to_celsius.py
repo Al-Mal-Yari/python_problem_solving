f52a0f0678aceaca30875989658caa6712b9a266
Al Mallari <almallari.dev@gmail.com>
2025-06-25 15:58:11 +0800

    add the incomplete given code

--- bootdev_convert_fahrenheit_to_celsius.py
+++ bootdev_convert_fahrenheit_to_celsius.py
@@ -9,3 +9,15 @@ In Fantasy Quest, characters lose health due to heat exhaustion. The game tracks
 Complete the to_celsius function body. It should return the temperature in Celsius for a given Fahrenheit temperature (f parameter).
 """
 
+def to_celsius(f):
+
+## Don't touch below this line
+
+def test(f):
+    c = round(to_celsius(f), 2)
+    print(f, "degrees fahrenheit is", c, "degrees celsius")
+
+test(100)
+test(88)
+test(104)
+test(112)
