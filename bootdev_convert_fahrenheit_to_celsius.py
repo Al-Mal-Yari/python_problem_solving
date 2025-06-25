72cacfd8fcb505870890af6bcd07f0fa0ffa506c
Al Mallari <almallari.dev@gmail.com>
2025-06-25 15:57:36 +0800

    Create bootdev_convert_fahrenheit_to_celsius.py

---
+++ bootdev_convert_fahrenheit_to_celsius.py
@@ -0,0 +1,11 @@
+"""
+Assignment
+Enough Discord talk, back to writing code.
+
+In Fantasy Quest, characters lose health due to heat exhaustion. The game tracks the temperature in Freedom units (Fahrenheit), but we need to display the temperature in Celsius for players outside the US. Here's the formula to calculate the temperature in Celsius from Fahrenheit (f):
+
+5/9 * (f - 32)
+
+Complete the to_celsius function body. It should return the temperature in Celsius for a given Fahrenheit temperature (f parameter).
+"""
+
